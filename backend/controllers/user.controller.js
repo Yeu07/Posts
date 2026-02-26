e9948457d5b5e9fc8a905591ba2962ada9019259
Yeu <yeumensilva9@gmail.com>
2026-02-26 12:46:38 -0300

    Create User Route

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,46 @@
+
+import { User } from "../src/models/user.model.js";
+
+const registerUser = async (req, res) => {
+    try {
+        const {userName, email, password} = req.body;
+
+        //Validation
+
+        if(!userName || !email || !password){
+            return res.status(400).json({message: "ALL fields are required!"})
+        }
+
+        // Check existing user
+
+        const existing = await User.findOne({email: email.toLowerCase()})
+
+        if(existing){
+            return res.status(400).json({message: "User already exists!"})
+        }
+
+        // Create user
+
+        const user = await User.create({
+            userName,
+            email: email.toLowerCase(),
+            password,
+            loggedIn: false,
+        })
+
+        res.status(201).json({
+            message: "User regitered",
+            user: {id :user._id, email:user.email, userName: user.userName}
+        }
+        )
+
+    } catch (error) {
+        res.status(500).json({message: "Internal server error", error:error,message})
+    }
+}
+
+
+
+export {
+    registerUser
+}
--- backend/src/app.js
+++ backend/src/app.js
@@ -2,4 +2,12 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
+// routes
+
+import userRouter from './routes/user.routes.js'
+
+app.use("/api/v1/users", userRouter)
+
 export default app
