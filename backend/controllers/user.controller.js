0f850685e3d42e16ffd1e30746f0c2878fcca436
Yeu <yeumensilva9@gmail.com>
2026-02-26 13:22:52 -0300

    User Logout

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -77,9 +77,33 @@ const loginUser = async(req,res) => {
     }
 }
 
+const logoutUser = async(req,res) => {
+    try {
+        const {email} = req.body;
+
+        const user = await User.findOne({
+            email: email.toLowerCase()
+        })
+
+        if(!user) return res.status(404).json({
+            message: "User not found"
+        })
+
+        res.status(200).json({
+            message: "Logout successful"
+        })
+
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+}
 
 
 export {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 }
